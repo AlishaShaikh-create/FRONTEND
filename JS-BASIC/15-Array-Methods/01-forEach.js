73baa81cf3520499469a86d7eb7a0a51f4592c69
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-08-15 14:03:51 +0000

    forEach

---
+++ JS-BASIC/15-Array-Methods/01-forEach.js
@@ -0,0 +1,15 @@
+//forEach:
+//arr.forEach(some function definition or name)
+
+
+let arr=[1,2,3,4,5,6,7,8,9];
+// let print=function(ele){
+//     console.log(ele);
+// } 
+// arr.forEach(print);
+
+ //using arrow function
+ arr.forEach((ele)=>
+{
+    console.log(ele);
+});
