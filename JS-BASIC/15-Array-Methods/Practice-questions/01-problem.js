0a9a7c2a2e58e03d6300b9eb39a2e0ffce7fa982
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-08-16 07:09:51 +0000

    practice problem

---
+++ JS-BASIC/15-Array-Methods/Practice-questions/01-problem.js
@@ -0,0 +1,9 @@
+//square and sum the array elements using the arrow function and then find the average of the array
+let arr=[1,2,3,4,5];
+let newarr=arr.map((ele)=>{
+    return ele*ele;
+});
+console.log(newarr);
+let sum = newarr.reduce((acc,ele)=>acc+ele,0)
+let avg=sum/newarr.length;
+console.log(avg);
