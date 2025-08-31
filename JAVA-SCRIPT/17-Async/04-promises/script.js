31cff3a546d695dfcf6110ac536e036f54222798
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-08-31 11:44:15 +0000

    callback hell

--- JAVA-SCRIPT/17-Async/04-promises/script.js
+++ JAVA-SCRIPT/17-Async/04-promises/script.js
@@ -1,20 +1,46 @@
 //lets create a function that stores data in the database ,it actually do not store the data
 
-function storeData()
+function storeData(data,success,failure)
 {
     // if the internet speed is high then the data gets stored;
     //lets create a random function that stores the data
-     let data=10;
+    
     let internetspeed=Math.floor(Math.random()*10)+1;
     console.log(internetspeed);
     if(internetspeed>4)
     {
-         console.log("data got stored:",data);
+         success();
     }
      else{
-        console.log("data was not stored");
+        failure();
      }
    
     
 }
-storeData();
+//here we are trying to store the nested form of the data
+
+storeData(10,()=>{
+    console.log("data1 was stored");
+  storeData("Alisha",
+    ()=>{
+    console.log("data2 was saved"); 
+  },
+  ()=>
+{
+    console.log("weak connection");
+
+})
+    
+},
+()=>{
+  console.log("weak connection");
+});
+
+//this is the situation of call back hell
+//CALLBACK HELL:
+//Callback Hell is a situation in JavaScript where multiple nested callback functions make your code look like it’s been through a blender on the highest setting.
+// This typically occurs when dealing with asynchronous operations, such as making API requests or handling file I/O, where one operation depends on the result of another or previous One.
+// Here’s an example of what callback hell might look like:🥶
+
+//Handling  callback hell?
+//1.promises
