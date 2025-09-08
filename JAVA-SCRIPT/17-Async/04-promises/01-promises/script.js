573ed042c659745c27cd61c0dd252439e195a562
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-09-08 21:22:35 +0530

    created a promise

---
+++ JAVA-SCRIPT/17-Async/04-promises/01-promises/script.js
@@ -0,0 +1,34 @@
+// The promsie Object represent the eventual completion (or failure) of an asynchronosus operation and its resulting value.
+
+/**
+ *  
+     PROMISES:
+     /     \
+ resolve    reject
+ (fulfilled)   (not fulfilled)
+
+ */
+ 
+ function storeData(data)
+ {
+   return new Promise((resolve,reject)=>{
+    let internetSpeed=Math.floor(Math.random()*10)+1;
+    if(internetSpeed>5){
+         resolve("Success:data was saved");
+    }else{
+        reject("failure: data was not saved");
+    }
+   
+   })
+
+ }
+
+// States of the promise
+// Promises basically have 3 states:
+// fullfilled
+// rejected
+// pending
+
+// PROMISE :
+// promise can be created as 
+// new Promise(resolve,reject)
