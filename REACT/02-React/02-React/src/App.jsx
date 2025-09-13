873267c6475903aef2d6e69c0dc1019490f264f7
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-09-13 15:09:31 +0530

    react

---
+++ REACT/02-React/02-React/src/App.jsx
@@ -0,0 +1,26 @@
+// Creating  our first component
+// Component first letter should be capital
+// We can return only single html element.
+// It is good practice to write each component in the individual file does not matter how small or how big the component is
+
+import './App.css'
+
+import {Title,Number} from './Title.jsx'
+
+// With div we can render multiple things or write multiple things
+// Every component should be written in the seperate
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Title></Title>
+      <Number/>
+      
+      <h1>I am App component</h1>
+    </div>
+  )
+}
+
+export default App
+
---
+++ REACT/02-React/02-React/src/Title.jsx
@@ -0,0 +1,15 @@
+function Title()
+{
+  return <h1>Coding</h1>
+}
+
+function Number()
+{
+   return 5;
+}
+
+// export default Title; 
+
+//The default is used to export the single file where the export is used to export the multiple file 
+
+export {Title,Number};
