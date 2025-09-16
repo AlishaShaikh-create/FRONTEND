4a36b6684f393795c22154913943d4ff60624a83
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-09-16 21:34:33 +0530

    react

--- REACT/06-States-in-React/src/App.jsx
+++ REACT/06-States-in-React/src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
-import Counter from "./Counter.jsx"
+import Counter from "./Counter.jsx";
+import { useState } from 'react';
 function App()
 {
+   let [count,setCount]=useState(0);
+   let incCount=()=>{
+    // count=count+1
+    setCount(count+1); // this is not the right way of updating the value
+    console.log(count);
+
+   }
+
   return (
     <>
     <h1> States in React </h1>
-    <Counter/>
+    <h3> Count = {count}</h3>
+    <button onClick={incCount}>Increase count</button>
+    {/* <Counter/> */}
+
     </>
   )
 }
