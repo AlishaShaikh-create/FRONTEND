72c31eef3d9f8e95560d56cce6172026e08bd3d2
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-09-16 22:13:26 +0530

    like button created

--- REACT/06-States-in-React/src/App.jsx
+++ REACT/06-States-in-React/src/App.jsx
@@ -1,21 +1,24 @@
+import LikeButton from './Activity1.jsx';
 import './App.css'
 import Counter from "./Counter.jsx";
 import { useState } from 'react';
 function App()
 {
-   let [count,setCount]=useState(0);
-   let incCount=()=>{
-    // count=count+1
-    setCount(count+1); // this is not the right way of updating the value
-    console.log(count);
+  //  let [count,setCount]=useState(0);
+  //  let incCount=()=>{
+  //   // count=count+1
+  //   setCount(count+1); // this is not the right way of updating the value
+  //   console.log(count);
 
-   }
+  
 
   return (
     <>
-    <h1> States in React </h1>
-    <h3> Count = {count}</h3>
-    <button onClick={incCount}>Increase count</button>
+    <h1> States in React</h1>
+     <LikeButton/>
+
+
+
     {/* <Counter/> */}
 
     </>
