420e38d35d3bd7b49e954cc584ab12e32f01f9fa
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-09-19 08:17:22 +0530

    import-export

---
+++ BACKEND/05-import-export/01-import.js
@@ -0,0 +1,3 @@
+import {sum,mul} from "./02-export.js"
+console.log(sum(2,3));
+console.log(mul(2,3));
---
+++ BACKEND/05-import-export/02-export.js
@@ -0,0 +1,6 @@
+export let sum=(a,b)=>{
+    return a+b;
+}
+export const mul=(a,b)=>{
+    return a*b;
+}
