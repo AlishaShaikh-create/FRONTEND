b242da823c1bb7de938e666c0b3710cb92dd3835
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-09-29 00:09:35 +0530

    fetch

---
+++ JAVA-SCRIPT/18-API/02-Fetch/02-fetch.js
@@ -0,0 +1,52 @@
+let url = "https://icanhazdadjoke.com/";
+// fetch(url,
+//     {
+//         headers:{
+//             Accept:'application/json'
+
+
+//         }
+//     }
+// )
+// .then((res)=>{
+//     console.log(res);
+//     console.log("The promise got resolved");
+//    return res.json()
+   
+// })
+//  .then((data)=>{
+//         console.log(data);
+//     })
+   
+    
+// .catch((error)=>{
+//     console.log("The error is : ");
+//     console.log(error);
+// })
+
+// fetch(url,
+//     {
+//         headers:{
+//             Accept:'application/json'
+
+
+//         }
+//     }
+// )
+// .then((res)=>{
+//     return res.json();
+// })
+// .then((data1)=>{
+//     console.log("data1 " ,data1);
+//     return fetch(url, {
+//         headers:{
+//             Accept:'application/json'
+//         }
+//     })
+// })
+// .then((res)=>{
+//     return res.json();
+// })
+// .then((data1)=>{
+//     console.log("data1 " ,data1);
+// })
