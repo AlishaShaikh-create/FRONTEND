6b3861238f2c89698b2d440f7456ef9128f248fd
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-09-30 17:58:06 +0530

    props

--- REACT/00-Practice/practice/src/App.jsx
+++ REACT/00-Practice/practice/src/App.jsx
@@ -1,23 +1,6 @@
 import React from "react";
-let name ="Alisha";
-function Product()
-{
-  return(
-    <>
-    <h3>Product name</h3>
-    <p>Description of the prodcut </p>
-    </>
-  )
-}
 function App()
 {
-  return(
-    <>
-      <Product/>
-        <Product/>
-      <Product/>
 
-    </>
-  )
 }
 export default App;
