5666c54a7048ca38d5e40052e9e107c13994cbd3
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-09-30 21:32:25 +0530

    react props

--- REACT/00-Practice/practice/src/Product.jsx
+++ REACT/00-Practice/practice/src/Product.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+// import React from 'react'
 
-function Product({title,description,price,feature})
+// function Product({title,description,price,feature})
+// {
+//    let list = feature.map((ele)=>{
+//     <li>{ele}</li>
+//    })
+    
+//     return(
+//         <>
+//           <h1>{title}</h1>
+//           <h3>{price}</h3>
+//          <ul>{list}</ul>
+//         </>
+//     )
+// }
+
+// export default Product;
+
+function Product({title,price ,feature })
 {
-   let  list= feature.map((ele)=>
-    <li> {ele}</li>)
+  let list=feature.map((ele)=>{
+    return <li>{ele}</li>
+
+  });
+  let discount=price>30000 ? "Discount of 5%":null
     return(
-        <>
-          <h1>{title}</h1>
-          <p>{description}</p>
-          <h3>{price}</h3>
-         <ul>{list}</ul>
-        </>
+    
+        <div>
+             <h1>{title}</h1>
+             <h3>Price: {price}</h3>
+             <p>{list}</p>
+             <p>{discount}</p>
+            
+        </div>
     )
 }
-
 export default Product;
--- REACT/00-Practice/practice/src/ProductTab.jsx
+++ REACT/00-Practice/practice/src/ProductTab.jsx
@@ -1,15 +1,30 @@
-import React from "react";
-import Product from "./Product";
-// import './App.css '
+// import React from "react";
+// import Product from "./Product";
+// // import './App.css '
+// function ProductTab()
+// {
+//     let option =["hi-tech" ,"durable"]
+//     return (
+//         <>
+//         <Product title="Phone" description="This is latest model phone" price={30000} feature={option}/>
+//         <Product title="Laptop" description="This is latest model Laptop" price={789722}/>
+//         <Product title="Pen" description="This is latest model Pen" price={3}/>
+//         </>
+//     )
+// }
+// export default ProductTab;
+
+import Product from './Product.jsx';
 function ProductTab()
 {
-    let option =["hi-tech" ,"durable"]
-    return (
-        <>
-        <Product title="Phone" description="This is latest model phone" price={30000} feature={option}/>
-        <Product title="Laptop" description="This is latest model Laptop" price={789722}/>
-        <Product title="Pen" description="This is latest model Pen" price={3}/>
-        </>
-    )
+    let option =["hitech","durable","handy"];
+ return(
+     <>
+         <Product  title="book" price={20} feature={option}  />
+         <Product  title="Laptop" price={60000} feature={option}  />
+         <Product title="Phone" price={30000} feature={option}/>
+   </>
+ );
+
 }
 export default ProductTab;
